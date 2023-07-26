9d53545f0fad5460e7408d2b42f1a954bad08a17
JL <inmessionantjl@outlook.com>
2023-07-26 23:06:01 +0800

    binary_search

--- LeetCode_2023/leetcode_problems/704.二分查找.py
+++ LeetCode_2023/leetcode_problems/704.二分查找.py
@@ -15,5 +15,39 @@ def binary_search1(nums: List[int], target: int) -> int:
     return -1
 
 
+def binary_search2_left(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l < r:
+        mid = l + (r - l) // 2
+        if nums[mid] < target:
+            l = mid + 1
+        else:
+            r = mid
+
+    if nums[l] == target:
+        return l
+
+    return -1
+
+
+def binary_search2_right(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l < r:
+        mid = l + (r - l + 1) // 2
+        if target < nums[mid]:
+            r = mid - 1
+        else:
+            l = mid
+
+    if nums[r] == target:
+        return r
+
+    return -1
+
+
 sorted_list = [1, 2, 3, 4, 7, 9, 10]
-print("target= 9, idx=", binary_search1(sorted_list, 9))
+print("binary_search1:        target= 9,  idx=", binary_search1(sorted_list, 9))
+print("binary_search2_left:   target= 9,  idx=", binary_search2_left(sorted_list, 9))
+print("binary_search2_right:  target= 9,  idx=", binary_search2_right(sorted_list, 9))
