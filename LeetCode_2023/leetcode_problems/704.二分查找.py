ff658b5d2f4b0f79988ef1dd7caad969cb041a0c
JL <inmessionantjl@outlook.com>
2023-07-28 22:42:26 +0800

    update

--- LeetCode_2023/leetcode_problems/704.二分查找.py
+++ LeetCode_2023/leetcode_problems/704.二分查找.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 def binary_search1(nums: List[int], target: int) -> int:
+
     l, r = 0, len(nums) - 1
 
     while l <= r:
@@ -16,6 +17,7 @@ def binary_search1(nums: List[int], target: int) -> int:
 
 
 def binary_search2_left(nums: List[int], target: int) -> int:
+
     l, r = 0, len(nums) - 1
 
     while l < r:
@@ -32,6 +34,7 @@ def binary_search2_left(nums: List[int], target: int) -> int:
 
 
 def binary_search2_right(nums: List[int], target: int) -> int:
+    
     l, r = 0, len(nums) - 1
 
     while l < r:
