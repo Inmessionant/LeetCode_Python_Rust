4be20be19bdb98296103f76d4333f04442b6ff84
JL <inmessionantjl@outlook.com>
2023-08-06 20:39:17 +0800

    merge

---
+++ LeetCode_2023/leetcode_problems/33. 搜索旋转排序数组.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        ...
+
+
+nums = [4,5,6,7,0,1,2]
+target = 0
+print(Solution().search(nums, target))
