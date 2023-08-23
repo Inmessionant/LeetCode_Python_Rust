60ab2a803b7586cb869cf346c703338c8c73ed6e
JL <inmessionantjl@outlook.com>
2023-08-23 20:25:34 +0800

    non_zero

--- LeetCode_2023/leetcode_problems/283. 移动零.py
+++ LeetCode_2023/leetcode_problems/283. 移动零.py
@@ -3,19 +3,17 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        len_nums, pre_non_zero = len(nums), -1
 
-        for idx, cur_value in enumerate(nums):
-            if cur_value != 0:
-                pre_non_zero += 1
-                nums[pre_non_zero], cur_value = cur_value, nums[pre_non_zero]
+        len_nums, left = len(nums), -1
 
-        pre_non_zero += 1
+        for idx, right in enumerate(nums):
+            if right != 0:
+                left += 1
+                nums[left], right = right, nums[left]
 
-        for i in range(pre_non_zero, len_nums):
+        left += 1
+
+        for i in range(left, len_nums):
             nums[i] = 0
 
         return nums
