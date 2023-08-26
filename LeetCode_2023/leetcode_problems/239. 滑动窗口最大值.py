278a4263036e00bcbbb8b0d299810eb011dd2562
JL <inmessionantjl@outlook.com>
2023-08-26 23:27:02 +0800

    sliding-windows-maximum

---
+++ LeetCode_2023/leetcode_problems/239. 滑动窗口最大值.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+
+        
+
+
+nums = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+print(Solution().maxSlidingWindow(nums, k))
