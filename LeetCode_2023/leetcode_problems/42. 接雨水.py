efe307a0f26e58ed6e4f7adb5d900b39cc584f15
l00661421 <lijunjie94@hisilicon.com>
2023-09-04 20:36:34 +0800

    接雨水

--- LeetCode_2023/leetcode_problems/42. 接雨水.py
+++ LeetCode_2023/leetcode_problems/42. 接雨水.py
@@ -3,7 +3,24 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        ...
 
-height = [0,1,0,2,1,0,1,3,2,1,2,1]
+        stack = []
+        n = len(height)
+        res = 0
+        for i in range(n):
+            while stack and height[i] > height[stack[-1]]:
+                bottom = height[stack.pop()]
+                if not stack: break
+                cur_left = height[stack[-1]]
+                cur_right = height[i]
+                height = min(cur_left, cur_right) - bottom
+                width = i - stack[-1] - 1
+                res += (width * height)
+
+            stack.append(i)
+
+        return res
+
+
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 print(Solution().trap(height))
