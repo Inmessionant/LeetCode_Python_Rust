e056936d6d6ec17a638db5f6e9482b11d67ebee8
JL <inmessionantjl@outlook.com>
2023-09-04 23:33:29 +0800

    1

---
+++ LeetCode_2023/leetcode_problems/84. 柱状图中最大的矩形.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+
+        ...
+
+
+heights = [2, 1, 5, 6, 2, 3]
+print(Solution().largestRectangleArea(heights))
