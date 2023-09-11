624f9f669ce94fda7dbac004e473288d0d208814
JL <inmessionantjl@outlook.com>
2023-09-11 23:46:31 +0800

    IPO

--- LeetCode_2023/leetcode_problems/502. IPO.py
+++ LeetCode_2023/leetcode_problems/502. IPO.py
@@ -1,9 +1,27 @@
+import heapq
+from pprint import pprint
 from typing import List
 
 
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        ...
 
-k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+        if w >= max(capital):
+            return w + sum(heapq.nlargest(k, profits))
+
+        heap = []  # 存储当前可用利益最大的
+        projects = sorted(zip(profits, capital), key=lambda x: x[1])  # 起始需要的资本从小到大
+
+        for i in range(k):
+            while projects and w >= projects[0][1]:
+                heapq.heappush(heap, -projects.pop(0)[0])
+            if heap:
+                w -= heapq.heappop(heap)
+        return w
+
+
+k = 2
+w = 0
+profits = [1, 2, 3]
+capital = [0, 1, 1]
 print(Solution().findMaximizedCapital(k, w, profits, capital))
