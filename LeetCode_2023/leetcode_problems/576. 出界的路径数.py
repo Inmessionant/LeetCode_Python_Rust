c3d366a4c4d6faac9757b151d9f7f48a2d836d5e
JL <inmessionantjl@outlook.com>
2023-09-25 22:31:55 +0800

    576. 出界的路径数

---
+++ LeetCode_2023/leetcode_problems/576. 出界的路径数.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        ...
+
+
+m = 2
+n = 2
+maxMove = 2
+startRow = 0
+startColumn = 0
+print(Solution().findPaths(m, n, maxMove, startRow, startColumn))  # res = 6
