9a5dda31dd2eccb79e49c17d90c14b46e8f9c677
JL <inmessionantjl@outlook.com>
2023-10-25 23:22:47 +0800

    Update 1005. K 次取反后最大化的数组和.py

---
+++ LeetCode_2023/leetcode_problems/1005. K 次取反后最大化的数组和.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        ...
+
+
+nums = [3, -1, 0, 2]
+k = 3
+print(Solution().largestSumAfterKNegations(nums, k))
