7e49259d844c2e05475d9a5ce27ce2bf49a804a4
JL <inmessionantjl@outlook.com>
2023-10-27 14:54:46 +0800

    Update 2023-09.08-1 浏览历史.py

---
+++ 软件开发能力认证/2023-09.08-1 浏览历史.py
@@ -0,0 +1,14 @@
+# 答题框内的代码仅为待实现代码，执行或提交代码时，仅包含待实现部分，不要包含其它代码。
+
+class BrowserHistorySys:
+    def __init__(self, homepage: str, max_count: int):
+        pass
+
+    def visit(self, url: str) -> int:
+        return 0
+
+    def back(self) -> str:
+        return ""
+
+    def forward(self) -> str:
+        return ""
