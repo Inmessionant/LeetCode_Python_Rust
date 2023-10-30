add40e590705c992a1273dbc86a0bd23b794f8fa
JL <inmessionantjl@outlook.com>
2023-10-30 10:57:41 +0800

    1

--- 软件开发能力认证/2023-08.11-1. 双遥控电路.py
+++ 软件开发能力认证/2023-08.11-1. 双遥控电路.py
@@ -5,3 +5,8 @@ class Solution:
 
     def control_circuit(self, records: List[List[int]], limit: int) -> int:
         ...
+
+
+records = [[0, 1], [1, 3], [2, 2], [3, 3], [4, 1], [4, 3]]
+limit = 3
+print(Solution().control_circuit(records, limit))
