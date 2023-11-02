eb03bac2de18754657219086c95a28945f1295ba
JL <inmessionantjl@outlook.com>
2023-11-03 00:17:31 +0800

    79. 单词搜索

--- LeetCode_2023/leetcode_problems/79. 单词搜索.py
+++ LeetCode_2023/leetcode_problems/79. 单词搜索.py
@@ -2,59 +2,39 @@ from functools import lru_cache
 from typing import List
 
 
-class Solution(object):
-    # 定义上下左右四个行走方向
-    directs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
 
-    def exist(self, board, word):
-        """
-        :type board: List[List[str]]
-        :type word: str
-        :rtype: bool
-        """
-        m = len(board)
-        if m == 0:
-            return False
-        n = len(board[0])
-        mark = [[0 for _ in range(n)] for _ in range(m)]
+        # k位置开始的word剩余字符，是否与(i, j)开始的board匹配(visited存储对应位置是否已经使用)
+        def backtrack(i, j, k, visited):
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    # 将该元素标记为已使用
-                    mark[i][j] = 1
-                    if self.backtrack(i, j, mark, board, word[1:]) == True:
+            if board[i][j] != word[k]: return False
+
+            if k == len(word) - 1: return True
+
+            for x, y in directs:
+                curx, cury = x + i, y + j
+                if 0 <= curx < rows and 0 <= cury < cols and not visited[curx][cury]:
+                    visited[curx][cury] = True
+                    if backtrack(curx, cury, k + 1, visited):
                         return True
-                    else:
-                        # 回溯
-                        mark[i][j] = 0
-        return False
+                    visited[curx][cury] = False
+            return False
 
-    def backtrack(self, i, j, mark, board, word):
-        if len(word) == 0:
-            return True
+        directs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        rows, cols = len(board), len(board[0])
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
 
-        for direct in self.directs:
-            cur_i = i + direct[0]
-            cur_j = j + direct[1]
-
-            if cur_i >= 0 and cur_i < len(board) and cur_j >= 0 and cur_j < len(board[0]) and board[cur_i][cur_j] == \
-                    word[0]:
-                # 如果是已经使用过的元素，忽略
-                if mark[cur_i][cur_j] == 1:
-                    continue
-                # 将该元素标记为已使用
-                mark[cur_i][cur_j] = 1
-                if self.backtrack(cur_i, cur_j, mark, board, word[1:]) == True:
-                    return True
-                else:
-                    # 回溯
-                    mark[cur_i][cur_j] = 0
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0]:
+                    visited[i][j] = True
+                    if backtrack(i, j, 0, visited):
+                        return True
+                    visited[i][j] = False
         return False
 
 
-board = [["A","B","C","E"],
-         ["S","F","C","S"],
-         ["A","D","E","E"]]
-word = "ABCB"
+board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+word = "ABCCED"
 print(Solution().exist(board, word))
