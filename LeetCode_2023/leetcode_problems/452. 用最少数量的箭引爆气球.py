11a7491be16b5153fb1349bd7d8333b63991be58
JL <inmessionantjl@outlook.com>
2023-11-07 22:47:24 +0800

    452. 用最少数量的箭引爆气球

---
+++ LeetCode_2023/leetcode_problems/452. 用最少数量的箭引爆气球.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        ...
+
+
+points = [[1, 2], [3, 4], [5, 6], [7, 8]]
+print(Solution().findMinArrowShots(points))
