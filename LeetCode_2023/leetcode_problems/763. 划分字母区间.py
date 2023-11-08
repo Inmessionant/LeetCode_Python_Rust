1241d72634581313f2ef8ed4cba9334f2c1fc547
JL <inmessionantjl@outlook.com>
2023-11-08 11:18:58 +0800

    Create 763. 划分字母区间.py

---
+++ LeetCode_2023/leetcode_problems/763. 划分字母区间.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        ...
+
+
+s = "ababcbacadefegdehijhklij"
+print(Solution.partitionLabels(s))
