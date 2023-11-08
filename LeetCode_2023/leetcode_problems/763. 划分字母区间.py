7841d20c5fdf5fd2283763d69b4964d77b8f70eb
JL <inmessionantjl@outlook.com>
2023-11-08 11:36:14 +0800

    763. 划分字母区间

--- LeetCode_2023/leetcode_problems/763. 划分字母区间.py
+++ LeetCode_2023/leetcode_problems/763. 划分字母区间.py
@@ -3,8 +3,23 @@ from typing import List
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        ...
+
+        last_idx = dict()
+
+        for idx, st in enumerate(s): # 存储每个字符最晚出现的idx
+            last_idx[st] = idx
+
+        left, right = 0, 0
+        res = []
+
+        for idx, st in enumerate(s):
+            right = max(right, last_idx[st])  # 更新当前字符串片段中字符最晚出现的idx
+            if right == idx:
+                res.append(right - left + 1)
+                left = right + 1
+
+        return res
 
 
 s = "ababcbacadefegdehijhklij"
-print(Solution.partitionLabels(s))
+print(Solution().partitionLabels(s))
