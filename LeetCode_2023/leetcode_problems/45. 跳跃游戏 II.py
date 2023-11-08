c2e61e99875de89d62c4a076d65c241dfffe15d5
JL <inmessionantjl@outlook.com>
2023-11-08 10:40:24 +0800

    Update 45. 跳跃游戏 II.py

--- LeetCode_2023/leetcode_problems/45. 跳跃游戏 II.py
+++ LeetCode_2023/leetcode_problems/45. 跳跃游戏 II.py
@@ -3,7 +3,16 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        ...
+
+        min_step, cur_cover, next_cover = 0, 0, 0
+
+        for i in range(len(nums)):
+            if i > cur_cover:
+                min_step += 1
+                cur_cover = next_cover
+            next_cover = max(next_cover, i + nums[i])
+
+        return min_step
 
 
 nums = [2, 3, 1, 1, 4]
