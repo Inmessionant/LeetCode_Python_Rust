1a3a7b97baf793a8ca6e1cf5c241b70eacbb55e9
JL <inmessionantjl@outlook.com>
2023-11-14 14:27:10 +0800

    Create 1631. 最小体力消耗路径.py

---
+++ LeetCode_2023/leetcode_problems/1631. 最小体力消耗路径.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        ...
+
+
+heights = [[1,2,2],[3,8,2],[5,3,5]]
+print(Solution().minimumEffortPath(heights))
