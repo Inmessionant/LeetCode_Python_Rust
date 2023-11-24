3147967c6431e6c015c43b6de04cc88743fdd6d7
JL <inmessionantjl@outlook.com>
2023-11-24 15:52:41 +0800

    Update 2023-02.10-1 二叉树构建.py

--- 软件开发能力认证/2023-02.10-1 二叉树构建.py
+++ 软件开发能力认证/2023-02.10-1 二叉树构建.py
@@ -1,15 +1,21 @@
 # Definition for a binary tree node.
+import collections
 from typing import List, Optional
 
 
 class TreeNode:
-    def __init__(self, x, left=None, right=None):
+    def __init__(self, x, level, left=None, right=None):
         self.val = x
+        self.level = level
         self.left = left
         self.right = right
 
 
 class Solution:
+    def __init__(self):
+        self.node_map = collections.defaultdict(list)
+        root = TreeNode(-1, 0)
+        self.node_map[0].append(root)
 
     def create_tree(self, operations: List[List[int]]) -> Optional[TreeNode]:
         ...
