acdee9f3e8982134f2f4658d6c4de904fbe9223b
JL <inmessionantjl@outlook.com>
2023-12-07 16:20:11 +0800

    Update 2021-05.21-1 租房信息查询系统.py

--- 软件开发能力认证/2021-05.21-1 租房信息查询系统.py
+++ 软件开发能力认证/2021-05.21-1 租房信息查询系统.py
@@ -4,17 +4,20 @@ from typing import List
 class RentingSystem:
 
     def __init__(self):
-
         ...
 
     def add_room(self, id: int, area: int, price: int, rooms: int, address: List[int]) -> bool:
-
         ...
 
     def delete_room(self, id: int) -> bool:
-
         ...
 
     def query_room(self, area: int, price: int, rooms: int, address: List[int], order_by: List[List[int]]) -> List[int]:
-
         ...
+
+
+obj = RentingSystem()
+print(obj.add_room(3, 24, 200, 2, [0, 1]))
+print(obj.add_room(1, 10, 400, 2, [1, 0]))
+print(obj.query_room(1, 400, 2, [1, 1], [[3, 1], [2, -1]]))
+print(obj.delete_room(3))
