4a9195829634296b1fe31c3666588bd8dc8e8d16
JL <inmessionantjl@outlook.com>
2023-12-07 19:02:00 +0800

    Update 2021-05.21-1 租房信息查询系统.py

--- 软件开发能力认证/2021-05.21-1 租房信息查询系统.py
+++ 软件开发能力认证/2021-05.21-1 租房信息查询系统.py
@@ -17,7 +17,20 @@ class RentingSystem:
 
 
 obj = RentingSystem()
+
+# case1
 print(obj.add_room(3, 24, 200, 2, [0, 1]))
 print(obj.add_room(1, 10, 400, 2, [1, 0]))
 print(obj.query_room(1, 400, 2, [1, 1], [[3, 1], [2, -1]]))
 print(obj.delete_room(3))
+
+# case2
+# print(obj.delete_room(10))
+# print(obj.add_room(3, 24, 200, 2, [0, 1]))
+# print(obj.add_room(3, 24, 500, 2, [0, 1]))
+# print(obj.add_room(3, 27, 500, 4, [1, 1]))
+# print(obj.add_room(1, 27, 500, 4, [20, 43]))
+# print(obj.add_room(6, 35, 227, 4, [2, 4]))
+# print(obj.add_room(9, 20, 3540, 4, [4, 321]))
+# print(obj.query_room(25, 900, 4, [10, 1], [[1, 1], [2, -1], [3, 1]]))
+# print(obj.query_room(25, 900, 4, [10, 1], []))
