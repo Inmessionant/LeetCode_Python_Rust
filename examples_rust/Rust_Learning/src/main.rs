447c4d651e6cd07b08fbd30a4552037689c428bf
JL <inmessionantjl@outlook.com>
2024-05-23 10:03:27 +0800

    Update main.rs

--- examples_rust/Rust_Learning/src/main.rs
+++ examples_rust/Rust_Learning/src/main.rs
@@ -82,6 +82,64 @@ fn func3_3() {
 
 }
 
+fn func3_4() -> i32{
+    let x = 5;
+    println!("before x = {}", x);
+    let y = {
+        let x = 1;
+        println!("inside x = {}", x);
+        x + 3
+    };
+    println!("outside x = {}", x);
+    println!("outside y = {}", y);
+    5
+}
+
+fn func3_5() {
+    let number = 11;
+
+    if number < 0 {
+        println!("number < 0 number = {}", number);
+    }else if number < 10 {
+        println!("number < 10 number = {}", number);
+    } else {
+        println!("default number = {}", number);
+    }
+
+    let cur = if number < 100 { 5 } else { 6 };
+    println!(" cur = {}", cur);
+}
+
+
+fn func3_6() {
+    let mut cnt: i32 = 1000;
+    loop {
+        println!("cnt = {}", cnt);
+        cnt -= 1;
+        if cnt == 0 {
+            break;
+        }
+    }
+
+    cnt = 1000;
+
+    while (cnt > 0) {
+        println!("cnt = {}", cnt);
+        cnt -= 1;
+    }
+
+    let a = [10, 20, 30 ,40 ,50];
+
+    for val in a.iter() {
+        println!("val = {}", val);
+    }
+
+    for number in (1..5).rev() {
+        println!("number = {}", number);
+    }
+}
+
+
 fn first_world(s: &str) -> &str {
     let bytes = s.as_bytes();
 
@@ -93,7 +151,7 @@ fn first_world(s: &str) -> &str {
     &s[..]
 }
 
-fn func3_4() {
+fn func4_3() {
     let s = String::from("Hello world");
     let idx = first_world(&s[..]);
     println!("{}", idx);
