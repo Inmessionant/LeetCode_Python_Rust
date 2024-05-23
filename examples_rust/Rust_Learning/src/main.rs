333079d250add5df7a06b2df55f1f3484db1f621
JL <inmessionantjl@outlook.com>
2024-05-23 23:56:07 +0800

    1

--- examples_rust/Rust_Learning/src/main.rs
+++ examples_rust/Rust_Learning/src/main.rs
@@ -192,7 +192,16 @@ fn func5_1() {
     };
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+let black = Color(0, 0, 0);
+
+fn func5_2() {
+
+}
+
 fn main() {
-    func5_1();
+    func5_2();
 
 }
