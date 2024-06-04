26fcdc15963f99aa3c603e0cf3ab2e6ba820ff7a
JL <inmessionantjl@outlook.com>
2024-06-04 23:16:26 +0800

    update

--- examples_rust/Rust_Learning/src/lib.rs
+++ examples_rust/Rust_Learning/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
         fn seat_at_table() {}
     }
 
@@ -9,4 +9,9 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
 }
