29606e0c67188451c9a7abb7f2b24fd9349e1dec
JL <inmessionantjl@outlook.com>
2024-06-04 23:25:53 +0800

    update

--- examples_rust/Rust_Learning/src/lib.rs
+++ examples_rust/Rust_Learning/src/lib.rs
@@ -15,3 +15,41 @@ pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
     front_of_house::hosting::add_to_waitlist();
 }
+
+
+fn serve_order() {}
+
+mod back_of_house {
+    fn cook_order() {}
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_friut: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_friut: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup, // pub
+        Salad, // pub
+    }
+
+
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = STring::from("Wheat");
+    // meal.seasonal_friut
+}
