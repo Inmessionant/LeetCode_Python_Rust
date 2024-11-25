ba4433e0f9176cbe6044d6290bd98f5690c88108
JL <inmessionantjl@outlook.com>
2024-11-25 19:58:41 +0800

    update binarsearch

---
+++ examples_python/example_c++/704.二分查找.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) { // versin 1 [left, right]
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    int search2(vector<int>& nums, int target) { // version 2 [left, right)
+        return -1;
+    }
+};
+
+int main() {
+    int target = 4;
+    vector<int> nums = {1, 2, 3 ,4};
+    cout << "result idx: " << Solution().search(nums, target) << endl;
+    return 0;
+}
